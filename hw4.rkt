18a127775cead624fd9657759209af02b8fb5efd
quweikoala <quweiprotoss@163.com>
2021-11-01 23:31:29 +0800

    first two q

---
+++ hw4.rkt
@@ -0,0 +1,17 @@
+
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+;; put your code below
+
+(define (sequence low high stride)
+  (if (<= low high)
+      (cons low (sequence (+ low stride) high stride))
+      null
+  )
+)
+
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs)
+)
