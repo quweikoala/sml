e20ad52e6ad253ed4cb2774322be43cbb33db620
quweikoala <quweiprotoss@163.com>
2021-11-21 23:58:47 +0800

    ci

--- hw4.rkt
+++ hw4.rkt
@@ -15,3 +15,25 @@
 (define (string-append-map xs suffix)
   (map (lambda (x) (string-append x suffix)) xs)
 )
+
+(define (list-nth-mod xs n)
+  (cond [(> 0 n) (error "list-nth-mod: negative number")]
+        [(null? xs) ((error "list-nth-mod: empty list"))]
+        [#t (let* ([i (remainder n (length xs))])
+            (car (list-tail xs i)))]
+  )
+)
+
+(define (stream-for-n-steps s n)
+  (letrec ([f (lambda (s ans cur)
+                (let ([pr (s)])
+                   (if (> cur n)
+                       ans
+                       (f (cdr pr) (cons (car pr) ans) (+ cur 1)))))])
+  (reverse (f s null 1))
+  )
+ )
+
+(define powers-of-two
+(letrec ([f (lambda (x) (cons x (lambda () (f (* x 2)))))])
+(lambda () (f 2))))
